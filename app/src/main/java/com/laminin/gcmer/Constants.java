40ae22b48b0b8628636af55f861a55ace68212c3
FranklinMichael <franklin@FranklinMichael.local>
2015-08-31 10:04:01 +0530

    updating urls

--- app/src/main/java/com/laminin/gcmer/Constants.java
+++ app/src/main/java/com/laminin/gcmer/Constants.java
@@ -14,19 +14,11 @@ public class Constants {
     public static final String REGISTRATION_COMPLETE = "registrationComplete";
     public static final String NOTIFICATION_RECEIVED = "notificationReceived";
 
-    public static final String API_KEY = "AIzaSyDdXKKKmhYJeOAMK8ejqkbgrWVPs9Pq4Po";
-    public static final String PROJECT_NUMBER = "239654894291";
-    public static final String PROJECT_NAME = "GcmTest";
-    public static final String PROJECT_ID = "micro-shoreline-101018";
-
-    public static final String FETCH_ALL_USERS_URL = "https://gcmer.herokuapp.com/users";
-    public static final String GET_MESSAGES_URL = "https://gcmer.herokuapp.com/get_messages";
-    public static final String SEND_NOTIFICATIONS_URL = "https://gcmer.herokuapp.com/send_notification";
 
 //    10.0.0.6
 
 
-//    public static final String FETCH_ALL_USERS_URL = "http://10.0.0.6:3000/users";
-//    public static final String GET_MESSAGES_URL = "http://10.0.0.6:3000/get_messages";
-//    public static final String SEND_NOTIFICATIONS_URL = "http://10.0.0.6:3000/send_notification";
+    public static final String FETCH_ALL_USERS_URL = "http://10.0.0.6:3000/users";
+    public static final String GET_MESSAGES_URL = "http://10.0.0.6:3000/get_messages";
+    public static final String SEND_NOTIFICATIONS_URL = "http://10.0.0.6:3000/send_notification";
 }
